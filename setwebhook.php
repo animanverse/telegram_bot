3a4d1e7207ad1a1d8a1521f04713cd0e40711d05
animanverse <marcosgamingeesports@gmail.com>
2025-07-20 15:21:56 +0530

    Update setwebhook.php

--- setwebhook.php
+++ setwebhook.php
@@ -1,5 +1,5 @@
 <?php
-$botToken ='8119508260:AAHyCkfoucWWxiJrFbHIzIAJvQZbZKst8a0";
+$botToken ='8119508260:AAHyCkfoucWWxiJrFbHIzIAJvQZbZKst8a0';
 $webhookURL = "https://telegram-bot1-p1gk.onrender.com/webhook.php";
 
 $url = "https://api.telegram.org/bot$botToken/setWebhook?url=" . urlencode($webhookURL);
