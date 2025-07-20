facfab822c6ce430d5a2434ce8588330f2c0560e
animanverse <marcosgamingeesports@gmail.com>
2025-07-20 15:15:03 +0530

    Update setwebhook.php

--- setwebhook.php
+++ setwebhook.php
@@ -1,10 +1,8 @@
 <?php
-// /bot/setWebhook.php
+$botToken ='8119508260:AAHyCkfoucWWxiJrFbHIzIAJvQZbZKst8a0";
+$webhookURL = "https://telegram-bot1-p1gk.onrender.com/webhook.php";
 
-define('BOT_TOKEN', '8119508260:AAHyCkfoucWWxiJrFbHIzIAJvQZbZKst8a0');
-define('WEBHOOK_URL', 'https://Animanverse.free.nf/bot/webhook.php'); // Live domain!
-
-$url = "https://api.telegram.org/bot" . BOT_TOKEN . "/setWebhook?url=" . WEBHOOK_URL;
-
+$url = "https://api.telegram.org/bot$botToken/setWebhook?url=" . urlencode($webhookURL);
 $response = file_get_contents($url);
 echo $response;
+?>
